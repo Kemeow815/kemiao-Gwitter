08e541bf8612f191b0d7d7b0e1c493298ab05086
克喵Kemeow <kemiaofx@163.com>
2025-08-26 17:56:49 +0800

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,14 +2,14 @@ import { GwitterConfig } from '../types/global';
 
 let config: GwitterConfig = {
   request: {
-    token: process。env。VITE_GWITTER_TOKEN?.split('?') || [],
-    clientID: process。env。VITE_GWITTER_CLIENT_ID || '',
-    clientSecret: process。env。VITE_GWITTER_CLIENT_SECRET || '',
+    token: process.env.VITE_GWITTER_TOKEN?.split('?') || [],
+    clientID: process.env.VITE_GWITTER_CLIENT_ID || '',
+    clientSecret: process.env.VITE_GWITTER_CLIENT_SECRET || '',
     pageSize: 6,
     autoProxy:
       'https://cors-anywhere.azm.workers.dev/https://github.com/login/oauth/access_token',
-    owner: process。env。VITE_GWITTER_OWNER || 'Kemeow815',
-    repo: process。env.VITE_GWITTER_REPO || 'kemiao-pyq',
+    owner: process.env.VITE_GWITTER_OWNER || 'Kemeow815',
+    repo: process.env.VITE_GWITTER_REPO || 'kemiao-pyq',
   },
 
   app: {
